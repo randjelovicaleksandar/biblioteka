6490f429d634355e6822fe7893449ed098ce251f
randjelovicaleksandar <randjelovic8913@fon.bg.ac.rs>
2015-03-17 12:55:38 +0100

    Dodata logicka kontrola.

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -3,6 +3,9 @@ package biblioteka;
 import java.util.LinkedList;
 import java.util.concurrent.LinkedTransferQueue;
 
+import javax.management.RuntimeErrorException;
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
@@ -11,11 +14,20 @@ public class Biblioteka implements BibliotekaInterfejs {
 	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga==null) {
+			throw new RuntimeException("Knjiga ne sme biti null");
+		}
+		if(knjige.contains(knjiga)) {
+			throw new RuntimeException("Knjiga je vec u biblioteci");
+		}
 		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(!knjige.contains(knjiga)) {
+			throw new RuntimeException("Knjiga nije u biblioteci");
+		}
 		knjige.remove(knjiga);
 	}
 
@@ -23,6 +35,9 @@ public class Biblioteka implements BibliotekaInterfejs {
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, long ISBN,
 			String izdavac, Autor autor) {
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		if (naslov==null && ISBN<=0 && izdavac==null && autor==null) 
+			throw new RuntimeException("Morate uneti neki kriterijum");
 		
 		if (ISBN > 0) {
 			for (int i = 0; i < knjige.size(); i++) {
